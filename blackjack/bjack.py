8b93440ec8c3e8695c3564fd70468046d7a0173c
Michael Mba <mikeyzx@gmail.com>
2022-11-12 19:49:48 +0000

    updated bjack file

--- blackjack/bjack.py
+++ blackjack/bjack.py
@@ -13,15 +13,40 @@ def deal_card():
 player = []
 computer = []
 
-player.append(deal_card())
-computer.append(deal_card())
-print(player)
-print(computer)
-player.append(deal_card())
-computer.append(deal_card())
-print(player)
-print(computer)
-computer_score = computer[0] + computer[1]
-print(computer_score)
-player_score = player[0] + player[1]
-print(player_score)
+
+play = True
+
+
+while play:
+    start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+    player.append(deal_card())
+    player.append(deal_card())
+    computer.append(deal_card())
+    computer.append(deal_card())
+    print(player)
+    print(computer[0])
+    deal = print("Type 'y' to get another card, type 'n' to pass: ")
+    if deal == "n":
+        print(player)
+        print(computer)
+        computer_score = computer[0] + computer[1]
+        player_score = player[0] + player[1]
+        if player_score < computer_score:
+            print("Dealer wins!")
+        elif player_score == computer_score:
+            print("Draw!")
+        else:
+            print("Player wins!")
+        play = False
+    else:
+        player.append(deal_card())
+        print(player)
+
+# player.append(deal_card())
+# computer.append(deal_card())
+# print(player)
+# print(computer)
+# computer_score = computer[0] + computer[1]
+# print(computer_score)
+# player_score = player[0] + player[1]
+# print(player_score)
