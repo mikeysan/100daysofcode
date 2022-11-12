24e06279f96c508fc8ae6ad37185f27ef33ff60e
Michael Mba <mikeyzx@gmail.com>
2022-11-12 23:07:42 +0000

    Added to if statement logic

--- blackjack/bjack.py
+++ blackjack/bjack.py
@@ -30,27 +30,35 @@ else:
     print("Maybe you will play later. Bye!")
 
 while play:
-    deal = input("Type 'y' to get another card, type 'n' to pass: ")
-    if deal == "n":
-        print(f"Your cards: {player}, final score: {score}")
-        print(computer)
-        computer_score = sum(computer)
-        player_score = sum(player)
-        print(f"Your final hand: {player}, final score: {player_score}")
-        print(f"Computer's final hand: {computer}, final score: {computer_score}")
-        if player_score < computer_score:
-            print("Dealer wins!")
-        elif player_score == computer_score:
-            print("Draw!")
-        elif player_score > 21:
-            print("Dealer wins!")
-        else:
-            print("Player wins!")
+    if score > 21:
+        print(f"Your final hand: {player}, final score: {score}")
+        print(f"Computer's final hand: {computer}, final score: {score}")
+        print("Dealer wins!")
         play = False
     else:
-        if score > 21:
-            print("Dealer wins!")
+        deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if deal == "n":
+            print(f"Your cards: {player}, final score: {score}")
+            print(computer)
+            computer_score = sum(computer)
+            player_score = sum(player)
+            print(f"Your final hand: {player}, final score: {player_score}")
+            print(f"Computer's final hand: {computer}, final score: {computer_score}")
+            if player_score < computer_score:
+                print("Dealer wins!")
+            elif player_score == computer_score:
+                print("Draw!")
+            elif player_score > 21:
+                print("Dealer wins!")
+            else:
+                print("Player wins!")
+            play = False
         else:
-            player.append(deal_card())
-            score = sum(player)
-            print(f"Your cards: {player}, current score: {score}")
+            if score > 21:
+                print("Dealer wins!")
+            else:
+                player.append(deal_card())
+                score = sum(player)
+                print(f"Your cards: {player}, current score: {score}")
+
+
